ca2331ee164ee8299f00b4227593c09392b5a712
nishikawa <nishikawa_ak_na@outlook.jp>
2024-11-29 22:47:47 +0900

    Add ページスクロール作成

---
+++ 13-demo/js/script.js
@@ -0,0 +1,13 @@
+const getScrollPercent = () => {
+  const scrolled = window.scrollY;
+  //console.log( `${scrolled} スクロールされました`);
+  const pageHeight = document.documentElement.scrollHeight;
+  const viewHeight = document.documentElement.clientHeight;
+  //console.log(`ページの高さ: ${pageHeight}、表示領域の高さ: ${viewHeight}`);
+  const percentage = scrolled / (pageHeight - viewHeight) * 100;
+  //console.log(percentage);
+  const bar = document.getElementById('bar');
+  bar.style.width = `${percentage}%`
+}
+  
+window.addEventListener('scroll', getScrollPercent);
