669caaf5fdc3d9fec06bfef25a8aaa157d46ca6f
nishikawa <nishikawa_ak_na@outlook.jp>
2024-12-15 17:18:11 +0900

    Add LookBookページ作成

--- WebSite/js/script.js
+++ WebSite/js/script.js
@@ -49,3 +49,13 @@ window.addEventListener('load', () => {
     }
   );
 });
+
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+for ( let i = 0; i < thumbImages.length; i++){
+  thumbImages[i].addEventListener('mouseover', (event) => {
+    mainImage.src = event.target.src;
+    mainImage.animate({
+      opacity: [0,1]},500)
+  });
+}
