e04d5e40411118ac9d74e5715671ae06d853a046
nishikawa <nishikawa_ak_na@outlook.jp>
2024-12-22 14:05:15 +0900

    Add Shopページ作成

--- WebSite/js/script.js
+++ WebSite/js/script.js
@@ -99,3 +99,28 @@ menuClose.addEventListener('click', () => {
     menuItem.animate({opacity: [1,0]},menuOptions);
   });
 });
+
+//監視範囲にはいったらふわっと表示
+const animateFade = (entries,obs) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      // console.log(entry.target);
+      entry.target.animate({
+        opacity: [0,1],
+        filter: ['blur(.4rem)','blur(0)'],
+        translate: ['0 4rem', 0,],
+      },
+      {
+        duration: 2000,
+        easing: 'ease',
+        fill: 'forwards', 
+      })
+      obs.unobserve(entry.target);
+    }
+  })
+}
+const fadeObserver = new IntersectionObserver(animateFade);
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+})
